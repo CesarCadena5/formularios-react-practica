35b2d0e024bf13ed57d4a00a587c28d49c5744a1
CesarCadena5 <cesarojitos555@hotmail.com>
2021-11-04 11:30:33 -0500

    Practica de los formularios en React, usando hooks.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Input from './components/Inputs';
+import Button from './components/Button';
+import Tarjeta from './components/Tarjeta';
+import Container from './components/Container';
+import useFormulario from './hooks/useFormulario';
 
 function App() {
+  const [usuarios, setUsuarios] = useState([]);
+  const [formulario, handleChange, reset] = useFormulario({ nombre: '', apellido: '', email: '' });
+
+  const submit = (e) => {
+    e.preventDefault();
+    setUsuarios([
+      ...usuarios,
+      formulario
+    ]);
+    reset();
+  };
+  console.log(usuarios);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Tarjeta>
+        <div style={{padding: 20}}>
+          <form onSubmit={submit}>
+            <Input 
+              name='nombre' 
+              label='Nombre' 
+              value={formulario.nombre} 
+              onChange={handleChange}
+              placeholder='Nombre'
+            />
+
+            <Input 
+              name='apellido' 
+              label='Apellido' 
+              value={formulario.apellido} 
+              onChange={handleChange}
+              placeholder='Apellido'
+            />
+
+            <Input 
+              name='email' 
+              label='Email' 
+              value={formulario.email} 
+              onChange={handleChange}
+              placeholder='Email'
+            />
+            <Button>Enviar</Button>
+          </form>
+        </div>
+      </Tarjeta>
+      <Tarjeta>
+        <ul>
+          {usuarios.map((u, i) => 
+            <li key={i}> {` ${u.nombre} ${u.apellido} ${u.email} `} </li>  
+          )}
+        </ul>
+      </Tarjeta>
+    </Container>
   );
 }
 
---
+++ src/components/Tarjeta.js
@@ -0,0 +1,10 @@
+import './Tarjeta.css';
+const Tarjeta = ({children}) => {
+    return(
+        <div className='tarjeta'>
+            {children}
+        </div>
+    );
+}
+
+export default Tarjeta;
